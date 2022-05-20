75a5415dd943ef960eeb096871f81abd3ffcc2ec
Svetlana <lanavladi1605gmail.com>
2022-05-20 23:25:24 +0500

    feat: add the second variant of solution 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,27 @@
+// The first variant of solution
+
+// const fs = require('fs');
+// const path = require('path');
+
+// fs.readFile(
+//     path.join(__dirname, 'text.txt'),
+//     'utf-8',
+//     (err, data) => {
+//         if (err) throw err;
+//         console.log(data);
+//     }
+// );
+
+
+// The second variant of solution
+
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(
-    path.join(__dirname, 'text.txt'),
-    'utf-8',
-    (err, data) => {
-        if (err) throw err;
-        console.log(data);
-    }
-);
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
+stream.on('error', error => console.log('Error', error.message));
