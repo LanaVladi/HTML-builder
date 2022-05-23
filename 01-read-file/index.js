a96fbe1214f0732cb599baff77491053a2117bf2
Svetlana <lanavladi1605gmail.com>
2022-05-23 21:50:07 +0500

    refactor:  solution 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -12,16 +12,15 @@
 //     }
 // );
 
-
 // The second variant of solution
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const input = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
 
-let data = '';
+let data = "";
 
-stream.on('data', chunk => data += chunk);
-stream.on('end', () => console.log(data));
-stream.on('error', error => console.log('Error', error.message));
+input.on("data", (chunk) => (data += chunk));
+input.on("end", () => console.log(data));
+input.on("error", (error) => console.log("Error", error.message));
