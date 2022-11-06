e203c35ab5c289c57e31e57416c262f834e48711
Svetlana <lanavladi1605gmail.com>
2022-11-06 17:31:11 +0500

    refactor:  solution 04-copy-directory (replace double quote with single quote, add indents)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
-dirSrcPath = path.join('04-copy-directory', 'files');
-dirDestPath = path.join('04-copy-directory', 'files-copy');  
+const dirSrcPath = path.join('04-copy-directory', 'files');
+const dirDestPath = path.join('04-copy-directory', 'files-copy');
 
- fs.rm(dirDestPath, { recursive: true, force: true }, () => {
-  fs.mkdir((dirDestPath), { recursive: true }, () => {
-  fs.readdir((dirSrcPath), {withFileTypes: true}, (error, files) => {
-   if (error) {
- console.log(error);
-  }
-  files.forEach(file => {
-   if(file.isFile()) {
-        fs.copyFile(path.join(dirSrcPath, file.name), path.join(dirDestPath, file.name),
-         () => {
-        });
-     };
-   });
-});
-   });
+fs.rm(dirDestPath, { recursive: true, force: true }, () => {
+  fs.mkdir(dirDestPath, { recursive: true }, () => {
+    fs.readdir(dirSrcPath, { withFileTypes: true }, (error, files) => {
+      if (error) {
+        console.log(error);
+      }
+      files.forEach((file) => {
+        if (file.isFile()) {
+          fs.copyFile(
+            path.join(dirSrcPath, file.name),
+            path.join(dirDestPath, file.name),
+            () => {}
+          );
+        }
+      });
+    });
+  });
 });
