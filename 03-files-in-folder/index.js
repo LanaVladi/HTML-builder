9233795c4c58b639bd52dbf8ca29f829e34ec060
Svetlana <lanavladi1605gmail.com>
2022-11-06 17:29:54 +0500

    refactor: solution 03-files-in-folder (replace double quote with single quote, add indents)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,28 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-fs.readdir(path.join(__dirname, "secret-folder"), { withFileTypes: true }, (error, files) => {
+fs.readdir(
+  path.join(__dirname, 'secret-folder'),
+  { withFileTypes: true },
+  (error, files) => {
     if (error) {
-        console.log(error);
+      console.log(error);
     }
-    files.filter(file => !file.isDirectory()).forEach(file => {
-        fs.stat(path.resolve('03-files-in-folder', "secret-folder", file.name), 
-        (error, fileStat) => {
-            if (error) {}
-            fileName = file.name.split('.').shift();
-            fileSize = fileStat.size;
+    files
+      .filter((file) => !file.isDirectory())
+      .forEach((file) => {
+        fs.stat(
+          path.resolve('03-files-in-folder', 'secret-folder', file.name),
+          (error, fileStat) => {
+            if (error) {
+              console.log(error);
+            }
+            const fileName = file.name.split('.').shift();
+            const fileSize = fileStat.size;
             const extension = path.extname(file.name).replace('.', '');
             console.log(`${fileName} - ${extension} - ${fileSize} bytes`);
-        });
-    });
-});
+          }
+        );
+      });
+  }
+);
