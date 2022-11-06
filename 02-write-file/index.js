786a1dd34f5295147be24159eb2d72a66ee3c18a
Svetlana <lanavladi1605gmail.com>
2022-11-06 17:28:36 +0500

    refactor: solution 02-write-file (replace double quote with single quote, add indents)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,24 +1,26 @@
-const fs = require("fs");
-const path = require("path");
-const process = require("process");
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
 
-const output = fs.createWriteStream(path.join("02-write-file", "destination.txt"));
+fs.createWriteStream(path.join('02-write-file', 'destination.txt'));
 // создает файл 'destination.txt' в папке '02-write-file'
 
 const { stdin, stdout } = process;
-stdout.write("Good day! Please enter your text:\n");
-stdin.on("data", (data) => {
- process.on('SIGINT', () => {
-  stdout.write("Good bye!\n")
+stdout.write('Good day! Please enter your text:\n');
+stdin.on('data', (data) => {
+  process.on('SIGINT', () => {
+    stdout.write('Good bye!\n');
     process.exit();
-});
-  if (data.toString().trim() === "exit") {
-    stdout.write("Good bye!\n");
+  });
+  if (data.toString().trim() === 'exit') {
+    stdout.write('Good bye!\n');
     process.exit();
   }
 
   fs.appendFile(
-    path.join("02-write-file", "destination.txt"), data,function (error) {
+    path.join('02-write-file', 'destination.txt'),
+    data,
+    function (error) {
       if (error) throw error; // ошибка чтения файла, если есть
     }
   );
