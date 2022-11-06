e1e482132253eddd3ac166b02f8344970d8eb013
Svetlana <lanavladi1605gmail.com>
2022-11-06 17:12:15 +0500

    refactor: set up .eslintrc.js (replace 'ecmaVersion': 12 with 13; 'es2021' with 'es2022')

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   'env': {
     'commonjs': true,
-    'es2021': true,
+    'es2022': true,
     'node': true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
-    'ecmaVersion': 12
+    'ecmaVersion': 13
   },
   'rules': {
     'indent': [
