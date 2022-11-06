062f39e22fd2a4587627775a45e7628d80d3ba2e
Svetlana <lanavladi1605gmail.com>
2022-11-06 17:32:14 +0500

    refactor: solution 05-merge-styles (replace double quote with single quote, add indents)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,18 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
-const bundle = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
-fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (error, files) => {
+const bundle = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'bundle.css')
+);
+fs.readdir(
+  path.join(__dirname, 'styles'),
+  { withFileTypes: true },
+  (error, files) => {
     if (error) {
-        console.log(error);
+      console.log(error);
     }
-    files.forEach(file => {
-        files.filter(file => file.isFile())
-        if (path.extname(file.name) === '.css') {
-            fs.readFile(path.join(__dirname, 'styles', file.name), (error, data) => {
-                if (error) {}
-                bundle.write(data + '\n');
-            });
-        };
+    files.forEach((file) => {
+      files.filter((file) => file.isFile());
+      if (path.extname(file.name) === '.css') {
+        fs.readFile(
+          path.join(__dirname, 'styles', file.name),
+          (error, data) => {
+            if (error) {
+              console.log(error);
+            }
+            bundle.write(data + '\n');
+          }
+        );
+      }
     });
-});
+  }
+);
