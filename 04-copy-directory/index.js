481568e3ab4ccffa0a5eaccb12f25169ec8a62b4
Svetlana <lanavladi1605gmail.com>
2022-11-08 03:10:53 +0500

    refactor:  solution 04-copy-directory (add solution using promises)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,24 +1,54 @@
-const fs = require('fs');
+// // solution using callback
+
+// const fs = require('fs');
+// const path = require('path');
+
+// const dirSrcPath = path.join('04-copy-directory', 'files');
+// const dirDestPath = path.join('04-copy-directory', 'files-copy');
+
+// fs.rm(dirDestPath, { recursive: true, force: true }, () => {
+//   fs.mkdir(dirDestPath, { recursive: true }, () => {
+//     fs.readdir(dirSrcPath, { withFileTypes: true }, (error, files) => {
+//       if (error) {
+//         console.log(error);
+//       }
+//       files.forEach((file) => {
+//         if (file.isFile()) {
+//           fs.copyFile(
+//             path.join(dirSrcPath, file.name),
+//             path.join(dirDestPath, file.name),
+//             () => {}
+//           );
+//         }
+//       });
+//     });
+//   });
+// });
+
+// solution using promises
+
+const { rm, mkdir, readdir, copyFile } = require('fs/promises');
 const path = require('path');
 
-const dirSrcPath = path.join('04-copy-directory', 'files');
-const dirDestPath = path.join('04-copy-directory', 'files-copy');
+async function copyDir() {
+  const dirSrcPath = path.join('04-copy-directory', 'files');
+  const dirDestPath = path.join('04-copy-directory', 'files-copy');
 
-fs.rm(dirDestPath, { recursive: true, force: true }, () => {
-  fs.mkdir(dirDestPath, { recursive: true }, () => {
-    fs.readdir(dirSrcPath, { withFileTypes: true }, (error, files) => {
-      if (error) {
-        console.log(error);
+  try {
+    await rm(dirDestPath, { recursive: true, force: true });
+    await mkdir(dirDestPath, { recursive: true });
+    const files = await readdir(dirSrcPath, { withFileTypes: true });
+    files.forEach(async (file) => {
+      if (file.isFile()) {
+        await copyFile(
+          path.join(dirSrcPath, file.name),
+          path.join(dirDestPath, file.name)
+        );
       }
-      files.forEach((file) => {
-        if (file.isFile()) {
-          fs.copyFile(
-            path.join(dirSrcPath, file.name),
-            path.join(dirDestPath, file.name),
-            () => {}
-          );
-        }
-      });
     });
-  });
-});
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+copyDir();
