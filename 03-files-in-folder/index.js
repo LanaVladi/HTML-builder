ffb42938a05c5035f04860a378ccb54e88177826
Svetlana <lanavladi1605gmail.com>
2022-11-08 03:10:17 +0500

    refactor: solution 03-files-in-folder (add solution using promises)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,28 +1,56 @@
-const fs = require('fs');
+// solution using callback
+
+// const fs = require('fs');
+// const path = require('path');
+
+// const pathToFolder = path.join(__dirname, 'secret-folder');
+
+// fs.readdir(path.join(pathToFolder), { withFileTypes: true }, (error, files) => {
+//   if (error) {
+//     console.error(error);
+//   }
+//   files
+//     .filter((file) => !file.isDirectory())
+//     .forEach((file) => {
+//       fs.stat(path.resolve(pathToFolder, file.name), (error, fileStat) => {
+//         if (error) {
+//           console.error(error);
+//         }
+//         const fileName = file.name.split('.').shift();
+//         const fileSize = fileStat.size;
+//         const extension = path.extname(file.name).replace('.', '');
+//         console.log(`${fileName} - ${extension} - ${fileSize} bytes`);
+//       });
+//     });
+// });
+
+// solution using promises
+
+const { readdir, stat } = require('fs/promises');
 const path = require('path');
 
-fs.readdir(
-  path.join(__dirname, 'secret-folder'),
-  { withFileTypes: true },
-  (error, files) => {
-    if (error) {
-      console.log(error);
-    }
+async function readDir() {
+  const pathToFolder = path.join(__dirname, 'secret-folder');
+
+  try {
+    const files = await readdir(path.join(pathToFolder), {
+      withFileTypes: true,
+    });
     files
       .filter((file) => !file.isDirectory())
-      .forEach((file) => {
-        fs.stat(
-          path.resolve('03-files-in-folder', 'secret-folder', file.name),
-          (error, fileStat) => {
-            if (error) {
-              console.log(error);
-            }
-            const fileName = file.name.split('.').shift();
-            const fileSize = fileStat.size;
-            const extension = path.extname(file.name).replace('.', '');
-            console.log(`${fileName} - ${extension} - ${fileSize} bytes`);
-          }
-        );
+      .forEach(async (file) => {
+        const fileStat = await stat(path.resolve(pathToFolder, file.name));
+        const fileName = file.name.split('.').shift();
+        const fileSize = fileStat.size;
+        const extension = path.extname(file.name).replace('.', '');
+        const output = `${fileName} - ${extension} - ${fileSize} bytes`;
+        console.log(output);
       });
+  } catch (error) {
+    if (error) {
+      console.error(error);
+    }
   }
-);
+}
+
+readDir();
